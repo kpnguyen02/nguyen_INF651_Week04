8469155b64df67ee82e207b47ba029efed643050
kpnguyen02 <112967218+kpnguyen02@users.noreply.github.com>
2022-09-20 13:49:15 -0500

    Add files via upload

---
+++ Week04/main.java
@@ -0,0 +1,114 @@
+//Practice Assignment 1
+//Student ID: 66504826
+
+//Question 1
+
+//The variable myNumber should by defined.
+
+var myNumber;
+
+//Question 2
+
+//The variable myNumber should be type number.
+
+//The variable myNumber should be equal to 5.
+
+myNumber = 5;
+
+//Question 3
+
+//The variable myOtherNumber should by defined.
+
+//The variable myOtherNumber should be type number.
+
+var myOtherNumber;
+
+//The variable myOtherNumber should be equal to 5.
+
+myOtherNumber = 5;
+
+//Question 4
+
+//The variable myTotal should by defined.
+
+//The variable myTotal should be type number.
+
+var myTotal;
+
+//The variable myTotal should be equal to the product of myNumber and myOtherNumber.
+
+myTotal = myNumber * myOtherNumber;
+
+//Question 5
+
+//The variable myName should by defined.
+
+var myName;
+
+//The variable myName should be type string.
+
+myName = "Kathy Nguyen";
+
+//The variable myCombo should by defined.
+
+//The variable myCombo should be type string.
+
+var myCombo;
+
+//The variable myCombo should be equal to the sum of myNumber and myOtherNumber.
+
+myCombo = "myNumber + myOtherNumber";
+
+//Question 6
+
+//The variable myOtherCombo should by defined.
+
+//The variable myOtherCombo should be type number.
+
+var myOtherCombo;
+
+//The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.
+
+myOtherCombo = myNumber + myOtherNumber;
+
+//Question 7
+
+//The variable myRemainder should by defined.
+
+//The variable myRemainder should be type number.
+
+var myRemainder;
+
+//The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+
+var myRemainder = myNumber/5;
+
+//Question 8
+
+//The variable i should by defined.
+
+//The variable i should be type number.
+
+//The variable i should be equal to 2.
+
+var i = 2;
+
+//Question 9
+
+//The variable int should by defined.
+
+//The variable int should be type number.
+
+//The variable int should be equal to 4.
+
+var int = 4;
+
+//Question 10
+
+//The variable myBoolean should by defined.
+
+//The variable myBoolean should be type boolean.
+
+//The variable myBoolean should be false.
+
+var myBoolean = false;
